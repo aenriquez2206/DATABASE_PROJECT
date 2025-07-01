29dfe35ce17eb6534bb7d99037b46b5c3374b7ec
eduardo18sr <ex.saldana.r@gmail.com>
2025-07-01 11:29:31 -0500

    codigo final

--- db/migrations/20250613163128_insert_notificaciones.sql
+++ db/migrations/20250613163128_insert_notificaciones.sql
@@ -1,10 +1,10 @@
 --migrate:up 
 
-INSERT INTO notificaciones (id, estado, nombre) VALUES (1, False, 'Alias sint rerum suscipit');
-INSERT INTO notificaciones (id, estado, nombre) VALUES (2, True, 'Provident quas tenetur animi');
-INSERT INTO notificaciones (id, estado, nombre) VALUES (3, False, 'Dignissimos quos dolore');
-INSERT INTO notificaciones (id, estado, nombre) VALUES (4, False, 'Ad labore omnis');
-INSERT INTO notificaciones (id, estado, nombre) VALUES (5, False, 'Velit maiores harum aperiam incidunt');
+INSERT INTO notificaciones (id,  nombre) VALUES (1,  'Alias sint rerum suscipit');
+INSERT INTO notificaciones (id,  nombre) VALUES (2,  'Provident quas tenetur animi');
+INSERT INTO notificaciones (id,  nombre) VALUES (3,  'Dignissimos quos dolore');
+INSERT INTO notificaciones (id,  nombre) VALUES (4,  'Ad labore omnis');
+INSERT INTO notificaciones (id,  nombre) VALUES (5,  'Velit maiores harum aperiam incidunt');
 
 --migrate:down 
 
