6ab17521481a471b173c06e3793d01eefedd3a25
aenriquez2206 <arielcalle12345@gmail.com>
2025-07-01 12:53:51 -0500

    tablas oracle terminado (revisar dates)

--- db/migrations/20250607223532_create_tarjetas_corredor.sql
+++ db/migrations/20250607223532_create_tarjetas_corredor.sql
@@ -4,7 +4,7 @@ CREATE TABLE tarjetas_corredor (
   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   id_tarjeta_fisica INTEGER NOT NULL,
   saldo FLOAT NOT NULL,
-  id_perfil FLOAT NOT NULL,
+  id_perfil INTEGER NOT NULL,
   FOREIGN KEY (id_perfil) REFERENCES perfiles (id)
 );
 
--- oracle/oracle_schema.sql
+++ oracle/oracle_schema.sql
@@ -134,8 +134,10 @@ CREATE TABLE perfiles(
   celular NUMBER(9),
   DNI NUMBER(8),
 	nombre VARCHAR2(30),
-  apellidos VARCHAR(30),
-  fecha_naciemnto VARCHAR(10)
+  apellidos VARCHAR2(30),
+  fecha_nacimiento DATE,
+  correo VARCHAR2(100),
+  contraseña VARCHAR2(100)
 );
 
 ALTER TABLE perfiles ADD (
@@ -154,15 +156,280 @@ BEGIN
 END;
 /
 -- 8 estaciones
+CREATE TABLE estaciones(
+	id NUMBER(7) PRIMARY KEY,
+  nombre VARCHAR2(40),
+	id_distrito NUMBER(7),
+  FOREIGN KEY(id_distrito) REFERENCES distritos
+);
 
+ALTER TABLE estaciones ADD (
+  CONSTRAINT estaciones_pk PRIMARY KEY (ID));
+
+CREATE SEQUENCE estaciones_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER estaciones_pk 
+BEFORE INSERT ON estaciones 
+FOR EACH ROW
+
+BEGIN
+  SELECT estaciones_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
+/
 -- 9 tarjetas corredor
--- 10 metodos de pago
--- 11 tarjetas univesitaio
--- 12 tarjetas general
--- 13 transacciones pagos
--- 14 transacciones ingresos
--- 15 app billeteras digitales
--- 16 tarjeta
--- 17 viajes
--- 18 notificaciones perfiles
+CREATE TABLE tarjetas_corredor(
+	id NUMBER(7) PRIMARY KEY,
+  id_tarjeta_fisica NUMBER(10),
+  saldo FLOAT(10),
+  id_perfil NUMBER(7),
+	FOREIGN KEY(id_perfil) REFERENCES perfiles
+);
 
+ALTER TABLE tarjetas_corredor ADD (
+  CONSTRAINT tarjetas_corredor_pk PRIMARY KEY (ID));
+
+CREATE SEQUENCE tarjetas_corredor_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER tarjetas_corredor_pk 
+BEFORE INSERT ON tarjetas_corredor 
+FOR EACH ROW
+
+BEGIN
+  SELECT tarjetas_corredor_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
+/
+-- 10 metodos de pago
+CREATE TABLE metodo_pagos(
+	id NUMBER(7) PRIMARY KEY,
+	id_banco NUMBER(7),
+  id_perfil NUMBER(7),
+  FOREIGN KEY(id_perfil) REFERENCES perfiles,
+  FOREIGN KEY(id_banco) REFERENCES bancos
+);
+
+ALTER TABLE metodo_pagos ADD (
+  CONSTRAINT metodo_pagos_pk PRIMARY KEY (ID));
+
+CREATE SEQUENCE metodo_pagos_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER metodo_pagos_pk 
+BEFORE INSERT ON metodo_pagos 
+FOR EACH ROW
+
+BEGIN
+  SELECT metodo_pagos_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
+/
+-- 11 tarjetas univesitaio
+
+CREATE TABLE tarjetas_universitarios(
+	id NUMBER(7) PRIMARY KEY,
+  id_tarjeta_corredor NUMBER(10),
+	id_alumno NUMBER(10)
+  FOREIGN KEY(id_tarjeta_corredor) REFERENCES tarjetas_corredor,
+  FOREIGN KEY(id_alumno) REFERENCES alumnos
+);
+
+ALTER TABLE tarjetas_universitarios ADD (
+  CONSTRAINT tarjetas_universitarios_pk PRIMARY KEY (ID));
+
+CREATE SEQUENCE tarjetas_universitarios_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER tarjetas_universitarios_pk 
+BEFORE INSERT ON tarjetas_universitarios 
+FOR EACH ROW
+
+BEGIN
+  SELECT tarjetas_universitarios_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
+/
+
+-- 12 tarjetas general
+
+CREATE TABLE tarjetas_general(
+	id NUMBER(7) PRIMARY KEY,
+  id_tarjeta_corredor NUMBER(7),
+  FOREIGN KEY(id_tarjeta_corredor) REFERENCES tarjetas_corredor
+);
+
+ALTER TABLE tarjetas_general ADD (
+  CONSTRAINT tarjetas_general_pk PRIMARY KEY (ID));
+
+CREATE SEQUENCE tarjetas_general_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER tarjetas_general_pk 
+BEFORE INSERT ON tarjetas_general 
+FOR EACH ROW
+
+BEGIN
+  SELECT tarjetas_general_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
+/
+-- 13 transacciones pagos
+
+CREATE TABLE transacciones_pagos(
+	id NUMBER(7) PRIMARY KEY,
+  monto FLOAT,
+  fecha DATE,
+  id_tarjeta_corredor NUMBER(7),
+  FOREIGN KEY (id_tarjeta_corredor) REFERENCES tarjetas_corredor
+);
+
+ALTER TABLE transacciones_pagos ADD (
+  CONSTRAINT transacciones_pagos_pk PRIMARY KEY (ID));
+
+CREATE SEQUENCE transacciones_pagos_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER transacciones_pagos_pk 
+BEFORE INSERT ON transacciones_pagos 
+FOR EACH ROW
+
+BEGIN
+  SELECT transacciones_pagos_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
+/
+-- 14 transacciones ingresos
+
+CREATE TABLE transacciones_ingreso(
+	id NUMBER(7) PRIMARY KEY,
+  monto FLOAT,
+  fecha DATE,
+  id_tarjeta_corredor NUMBER(7),
+  id_metodo_pago NUMBER(7),
+  FOREIGN KEY (id_tarjeta_corredor) REFERENCES tarjetas_corredor,
+	FOREIGN KEY (id_metodo_pago) REFERENCES metodo_pagos
+);
+
+ALTER TABLE transacciones_ingreso ADD (
+  CONSTRAINT transacciones_ingreso_pk PRIMARY KEY (ID));
+
+CREATE SEQUENCE transacciones_ingreso_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER transacciones_ingreso_pk 
+BEFORE INSERT ON transacciones_ingreso 
+FOR EACH ROW
+
+BEGIN
+  SELECT transacciones_ingreso_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
+/
+-- 15 app billeteras digitales
+
+CREATE TABLE app_billeteras_digitales(
+	id NUMBER(7) PRIMARY KEY,
+	id_metodo_pago NUMBER(7),
+  id_nombre_app_billetera_digital NUMBER(7),
+	FOREIGN KEY (id_metodo_pago) REFERENCES metodo_pagos,
+  FOREIGN KEY (id_nombre_app_billetera_digital) REFERENCES nombre_app_billeteras_digitales
+);
+
+ALTER TABLE app_billeteras_digitales ADD (
+  CONSTRAINT app_billeteras_digitales_pk PRIMARY KEY (ID));
+
+CREATE SEQUENCE app_billeteras_digitales_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER app_billeteras_digitales_pk 
+BEFORE INSERT ON app_billeteras_digitales 
+FOR EACH ROW
+
+BEGIN
+  SELECT app_billeteras_digitales_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
+/
+
+-- 16 tarjeta
+CREATE TABLE tarjetas(
+	id NUMBER(7) PRIMARY KEY,
+  numero_tarjeta NUMBER(50),
+  fecha_caducidad DATE,
+  cvv NUMBER(7),
+  nombre_titular VARCHAR2(100),
+  id_metodo_pago NUMBER(7),
+  id_red_pago NUMBER(7),
+	FOREIGN KEY (id_metodo_pago) REFERENCES metodo_pagos,
+  FOREIGN KEY (id_red_pago) REFERENCES red_pagos
+);
+
+ALTER TABLE tarjetas ADD (
+  CONSTRAINT tarjetas_pk PRIMARY KEY (ID));
+
+CREATE SEQUENCE tarjetas_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER tarjetas_pk 
+BEFORE INSERT ON tarjetas 
+FOR EACH ROW
+
+BEGIN
+  SELECT tarjetas_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
+/
+-- 17 viajes
+
+CREATE TABLE viajes(
+	id NUMBER(7) PRIMARY KEY,
+	fecha DATE,
+  final_estacion_id NUMBER(7),
+  inicial_estacion_id NUMBER(7),
+  perfil_id NUMBER(7),
+  FOREIGN KEY (perfil_id) REFERENCES perfiles,
+  FOREIGN KEY (final_estacion_id) REFERENCES estaciones,
+  FOREIGN KEY (inicial_estacion_id) REFERENCES estaciones
+);
+
+ALTER TABLE viajes ADD (
+  CONSTRAINT viajes_pk PRIMARY KEY (ID));
+
+CREATE SEQUENCE viajes_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER viajes_pk 
+BEFORE INSERT ON viajes 
+FOR EACH ROW
+
+BEGIN
+  SELECT viajes_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
+/
+-- 18 notificaciones perfiles
+CREATE TABLE notificaciones_perfiles(
+	id NUMBER(7) PRIMARY KEY,
+  id_notificacion NUMBER(7),
+  id_perfil NUMBER(7),
+  FOREIGN KEY (perfil_id) REFERENCES perfiles,
+  FOREIGN KEY (id_notificacion) REFERENCES notificaciones
+);
+
+ALTER TABLE notificaciones_perfiles ADD (
+  CONSTRAINT notificaciones_perfiles_pk PRIMARY KEY (ID));
+
+CREATE SEQUENCE notificaciones_perfiles_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER notificaciones_perfiles_pk 
+BEFORE INSERT ON notificaciones_perfiles 
+FOR EACH ROW
+
+BEGIN
+  SELECT notificaciones_perfiles_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
+/
